99554b7f3b454ee6823161a0b9d6be7b77da0c8f
tanmally <tm@beyondbytes.co.in>
2013-07-23 19:36:09 +0530

    Modified route js file , all route setting goes into this file , added multi part file uplaod to mongo gridfs

--- config/express.js
+++ config/express.js
@@ -4,6 +4,9 @@
 
 var express = require('express'), fs = require('fs'), mongoose = require('mongoose'),
     http = require('http'), path = require('path'), passport = require('passport');
+var multipart = require('connect-multipart-gridform');
+
+
 
 // Bootstrap models
 var models_path = __dirname + '/../models';
@@ -23,14 +26,17 @@ app.set('views', __dirname + './../views');
 app.set('view engine', 'jade');
 // app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + './../public'));
 app.use(express.static(path.join(__dirname, './../public')));
-app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + "./../public/temp/" }))
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + "./../public/temp/" }));
+app.use(multipart({
+        db : config.db,
+        mongo : mongoose
+    }));
 
 
     passport.serializeUser(function(user, done) {
@@ -42,16 +48,7 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-
-
-var user = require('../controllers/user'), signin = require('../controllers/signin'),
-    template = require('../controllers/template');
-
-app.get('/', signin.signin);
-app.get('/users', user.list);
-app.post('/upload', template.upload);
-app.get('/template', template.template);
-
+ var route= require('./routes')(app,config);
 
 
 }
